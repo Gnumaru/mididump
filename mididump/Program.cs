2795963e6a3db691178a614cefeac6ce4ac0b71a
gnumaru <lucazfc@gmail.com>
2018-02-17 20:15:46 -0200

    .

--- mididump/Program.cs
+++ mididump/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using NAudio.Midi;
 
 namespace mididump {
     class MainClass {
         public static void Main(string[] args) {
-            foreach(var e in new MidiFile(args[0]).Events.SelectMany(e => e)) {
-                Console.WriteLine(e);
+            MidiFile mf = null;
+
+            if(args.Length > 0) mf = new MidiFile(args[0]);
+            else {
+                Stream stdin = Console.OpenStandardInput();
+                byte[] buffer = new byte[10 * 1024];
+                List<byte> bytes = new List<byte>(0);
+                int count = 0;
+                while((count = stdin.Read(buffer, 0, buffer.Length)) > 0) {
+                    bytes.AddRange(buffer.Take(count));
+                }
+                mf = new MidiFile(new MemoryStream(bytes.ToArray()), true);
             }
+
+            foreach(var e in mf.Events.SelectMany(e => e).OrderBy(e => e.AbsoluteTime)) Console.WriteLine(e);
         }
     }
 }
